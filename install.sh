55bd692c6ec652fecb3a64e7b10498a79104c0cd
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-31 21:03:04 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+echo "Flarum论坛一键搭建脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/flarum-on-replit"
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://github.com/sxbai/flarum-on-replit/raw/master/.replit
+wget -O replit.nix https://github.com/sxbai/flarum-on-replit/raw/master/replit.nix
+cd ..
+wget -O main.sh https://github.com/sxbai/flarum-on-replit/raw/master/main.sh
+wget -O sx https://down.sxbai.repl.co/alist/v3.15.0/alist3.15.0
+cp -r build/.replit . && cp -r build/replit.nix .
+echo "恭喜搭建完成"
+echo "点击Run立即运行项目"
+echo "查询用户名及密码运行下面命令"
+echo "./sx admin"
+rm -rf build/
