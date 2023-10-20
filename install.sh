e9532dc35071f6df41a0c8aaa02e1a151e1dc2e4
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-10-20 14:16:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,12 +11,19 @@ wget -O a.zip https://github.com/sxbai/flarum-on-replit/raw/master/a.zip
 nix-env -iA nixpkgs.unzip
 unzip a.zip
 cp -r a/.cache .cache
-nix-env -iA nixpkgs.php80Packages.composer
+nix-env -iA nixpkgs.php82Packages.composer
 mkdir public
 cd public
 composer create-project flarum/flarum .
 composer require flarum-lang/chinese-simplified
+echo "正在设置允许注册中文名"
+sed -i "s#a-z0-9_-#-_a-z0-9\\\x7f-\\\xff#" \
+vendor/flarum/core/src/User/UserValidator.php
+echo "正在设置允许 @ 中文名"
+sed -i "s#a-z0-9_-#-_a-zA-Z0-9\\\x7f-\\\xff#" \
+vendor/flarum/mentions/src/ConfigureMentions.php
 cd ..
+echo "设置完成！"
 cp -r build/.replit . && cp -r build/replit.nix .
 rm -rf build/ && rm -r a/
 echo "Flarum论坛一键搭建脚本"
